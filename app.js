f229ff2e0bced310eedde696617de42fdc401e02
Abdullah Shaikh <160311441+abdullahshaik697@users.noreply.github.com>
2024-10-04 20:27:49 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var arr = [
-    "./assets/1.jpg",
-    "./assets/2.jpg",
-    "./assets/3.jpg",
-    "./assets/4.jpg",
-    "./assets/5.jpg"
+    "https://encrypted-tbn0.gstatic.com/imagesq=tbn:ANd9GcSGjkXbED_Y4jEzRI2SzQXeL3hjZ_lkAuUu2g&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqpfePoJjLEOUbjnPVxdIwwonaAZRGcP5_5w&s",
+    "https://dynamic-media-cdn.tripadvisor.com/media/daodao/photo-o/14/e2/52/05/img-20180916-184638-largejpg.jpg?w=500&h=400&s=1",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEbXHbost5JWZXQ4dAWRucleSkn3EyLrM2mw&s",
+    "https://cdn.virily.com/wp-content/uploads/2017/12/Lahore_fort_with_Pakistani_Flag.jpg"
 ]
 
 console.log(arr);
@@ -33,3 +33,4 @@ function next(){
         nextBtn.disabled=true;
     }
 }
+  
