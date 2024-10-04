243e8fa42b725ce9dfe0f59b05ba928dbb07a668
Abdullah Shaikh <160311441+abdullahshaik697@users.noreply.github.com>
2024-10-05 02:30:56 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var arr = [
-    "https://encrypted-tbn0.gstatic.com/imagesq=tbn:ANd9GcSGjkXbED_Y4jEzRI2SzQXeL3hjZ_lkAuUu2g&s",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqpfePoJjLEOUbjnPVxdIwwonaAZRGcP5_5w&s",
+    "https://media.istockphoto.com/id/1386446426/photo/badshahi-mosque.jpg?s=612x612&w=0&k=20&c=vShhc9rb17q_5k-tx_HJnlDvlE4YjCNNlOCEWplI2_Y=",
+    "https://zameenblog.s3.amazonaws.com/blog/wp-content/uploads/2020/08/Mazar-e-Quaid-B-14-08-1024x640.jpg",
     "https://dynamic-media-cdn.tripadvisor.com/media/daodao/photo-o/14/e2/52/05/img-20180916-184638-largejpg.jpg?w=500&h=400&s=1",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEbXHbost5JWZXQ4dAWRucleSkn3EyLrM2mw&s",
+    "https://tribune-reloaded.s3.amazonaws.com/media/images/2162950-minarepakistanx-1582517159/2162950-minarepakistanx-1582517159.jpg",
     "https://cdn.virily.com/wp-content/uploads/2017/12/Lahore_fort_with_Pakistani_Flag.jpg"
 ]
 
